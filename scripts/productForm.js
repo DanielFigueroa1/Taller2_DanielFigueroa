fd35a5941ab768b5c95264357c6565d9b6456d73
DanielFigueroa1 <daniel.alejandroolmedo@outlook.es>
2021-04-17 02:37:43 -0500

    Creación de categorias adicionales y implementacion de firestore

--- scripts/productForm.js
+++ scripts/productForm.js
@@ -1,3 +1,17 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAmMYPMqf523SNFw2MbXPGdokIXwe-suN4",
+    authDomain: "web-store-deadbehind.firebaseapp.com",
+    projectId: "web-store-deadbehind",
+    storageBucket: "web-store-deadbehind.appspot.com",
+    messagingSenderId: "527773743903",
+    appId: "1:527773743903:web:7b0dc4a81f6d15c98ace2a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const db = firebase.firestore();
+
+
 const productForm = document.querySelector(".productForm");
 
 productForm.addEventListener("submit", function(event) {
@@ -7,14 +21,36 @@ productForm.addEventListener("submit", function(event) {
     console.log("size s: ", productForm.size_s.value);
     console.log("size m: ", productForm.size_m.value);
     console.log("size l: ", productForm.size_l.value);
+    console.log("colorW: ", productForm.colorW.value);
+    console.log("colorB: ", productForm.colorB.value);
+    console.log("shirt: ", productForm.shirt.value);
+    console.log("sweater: ", productForm.sweater.value);
+    console.log("mug: ", productForm.mug.value);
+    console.log("sticker: ", productForm.sticker.value);
+    console.log("pin: ", productForm.pin.value);
+    console.log("popularity: ", productForm.popularity.value);
     
 
     const product = {
         name: productForm.name.value,
         price: parseFloat(productForm.price.value),
-        sizes: []
+        sizes: [],
+        color: [],
+        type: [],
+        popularity: parseFloat(productForm.price.value)
     };
+    //chequeo tamaños
     if(productForm.size_s.checked) product.sizes.push("s");
-    if(productForm.size_s.checked) product.sizes.push("m");
-    if(productForm.size_s.checked) product.sizes.push("l");
+    if(productForm.size_m.checked) product.sizes.push("m");
+    if(productForm.size_l.checked) product.sizes.push("l");
+
+    //chequeo color
+    if(productForm.colorW.checked) product.sizes.push("white");
+    if(productForm.colorB.checked) product.sizes.push("black");
+    //chequeo tipo de mercancia
+    if(productForm.shirt.checked) product.sizes.push("camisa");
+    if(productForm.sweater.checked) product.sizes.push("sweater");
+    if(productForm.mug.checked) product.sizes.push("taza");
+    if(productForm.sticker.checked) product.sizes.push("sticker");
+    if(productForm.pin.checked) product.sizes.push("pin");
 });
