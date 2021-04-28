8fc2988fff42b2f8998544b6bc538413c0513480
DanielFigueroa1 <daniel.alejandroolmedo@outlook.es>
2021-04-28 02:48:45 -0500

    creacion union database de shop

--- scripts/shop.js
+++ scripts/shop.js
@@ -3,11 +3,21 @@ db.collection("products")
     .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
+            const data = doc.data();
             const product = document.createElement("a");
             product.innerHTML = `
-            <img class="product__img src="${item.img}" alt="">
+            <img class="product__img src="${data.imgages[0].url}" alt="">
+            <div class="producto__info">
+                <h3>
+                    ${data.name}
+                </h3>
+                <h4 class ="product__price"> ${data.price}</h3>
+                </div>
             `;
+            product.classList.add("product");
+            product.setAttribute("href","#")
+
+            list.appendChild(product);
 
         });
     })
