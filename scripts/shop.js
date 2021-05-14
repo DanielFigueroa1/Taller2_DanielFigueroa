89797c97b616334388b126059ab10cec1329a912
DanielFigueroa1 <daniel.alejandroolmedo@outlook.es>
2021-05-13 21:36:25 -0500

    Filters iniciados
    Se iniciaron los filters pero por problemas con la forma que se ilustran los elementos tocara ir a monitorias o con el profe.

--- scripts/shop.js
+++ scripts/shop.js
@@ -1,27 +1,42 @@
 const list = document.querySelector(".list");
+
+const filterOptions = document.querySelector(".filterOptions");
+
 let products = [{
     URLimagen: "https://firebasestorage.googleapis.com/v0/b/web-store-deadbehind.appspot.com/o/shirt%2FshirtSkullW.png?alt=media&token=5da89ba6-5cae-41c1-b380-298449106247",
     color: ["white", "black"],
     name: "Camisa Calavera",
-    popularity: 5,
-    price: 40000 ,
+    popularity: 3,
+    price: 30000,
     sizes: ["s", "m", "l"],
     type: "shirt",
 },
 {
-    /*URLimagen: "https://firebasestorage.googleapis.com/v0/b/web-store-deadbehind.appspot.com/o/shirt%2FshirtSkullW.png?alt=media&token=5da89ba6-5cae-41c1-b380-298449106247",
+    URLimagen: "https://firebasestorage.googleapis.com/v0/b/web-store-deadbehind.appspot.com/o/shirt%2FshirtCatW.png?alt=media&token=36c12614-4c37-4606-95b6-cc38df2f7ec4",
     color: ["white", "black"],
-    name: "Camisa Calavera",
-    popularity: 5,
-    price: 40000 ,
+    name: "Camisa Gato",
+    popularity: 4,
+    price: 25000,
     sizes: ["s", "m", "l"],
-    type: "shirt",*/
+    type: "shirt",
+},
+{
+    URLimagen: "https://firebasestorage.googleapis.com/v0/b/web-store-deadbehind.appspot.com/o/sweater%2FsweaterZombieW.png?alt=media&token=1c4303d5-81a8-481c-ae70-c2341a0d3f19",
+    color: ["white", "black"],
+    name: "Sueter Cabeza <br>Zombie 1",
+    popularity: 5,
+    price: 40000,
+    sizes: ["s", "m", "l"],
+    type: "sweater",
 }];
+
+
+
 
 function printElements(data) {
     data.forEach(element => {
         const data = element;
-        const product = document.createElement("a");
+        const product = document.createElement("a"); //la a lo hace un link
             product.innerHTML = `
             <img class="shirt__product__img" src="${data.URLimagen}" alt="">
             <div class="product__name">
@@ -74,6 +89,7 @@ function handleCloseModal2(){
 loginCancel.addEventListener("click", handleCloseModal);
 registerCancel.addEventListener("click", handleCloseModal2);
 
+
 /*modalLoginUser.forEach(e => {
     e.addEventListener("click", handleOpenModal); //por cada elemento lo va a abrir
 }); si fuera a hacer un modal desde la misma clase*/ 
@@ -81,4 +97,12 @@ registerCancel.addEventListener("click", handleCloseModal2);
 modalLoginUser.addEventListener("click", handleOpenModal); //fin login
 modalRegisterUser.addEventListener("click", handleOpenModal2);
 
+filterOptions.type.addEventListener("change", function (){
+    console.log(filterOptions.type.value);
+
+    db.collection("products")
+    .where("type","==", filterOptions.type.value)
+    .get()
+    .then(handleCollectionResult);
+});
 
