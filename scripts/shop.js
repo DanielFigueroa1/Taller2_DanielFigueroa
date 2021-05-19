b3192ce4c0444601f7d7b38227a161f1ee84371d
DanielFigueroa1 <daniel.alejandroolmedo@outlook.es>
2021-05-18 21:03:24 -0500

    Aplicacion nuevo metodo de database y filtros y orden

--- scripts/shop.js
+++ scripts/shop.js
@@ -2,63 +2,85 @@ const list = document.querySelector(".list");
 
 const filterOptions = document.querySelector(".filterOptions");
 
-let products = [{
-    URLimagen: "https://firebasestorage.googleapis.com/v0/b/web-store-deadbehind.appspot.com/o/shirt%2FshirtSkullW.png?alt=media&token=5da89ba6-5cae-41c1-b380-298449106247",
-    color: ["white", "black"],
-    name: "Camisa Calavera",
-    popularity: 3,
-    price: 30000,
-    sizes: ["s", "m", "l"],
-    type: "shirt",
-},
-{
-    URLimagen: "https://firebasestorage.googleapis.com/v0/b/web-store-deadbehind.appspot.com/o/shirt%2FshirtCatW.png?alt=media&token=36c12614-4c37-4606-95b6-cc38df2f7ec4",
-    color: ["white", "black"],
-    name: "Camisa Gato",
-    popularity: 4,
-    price: 25000,
-    sizes: ["s", "m", "l"],
-    type: "shirt",
-},
-{
-    URLimagen: "https://firebasestorage.googleapis.com/v0/b/web-store-deadbehind.appspot.com/o/sweater%2FsweaterZombieW.png?alt=media&token=1c4303d5-81a8-481c-ae70-c2341a0d3f19",
-    color: ["white", "black"],
-    name: "Sueter Cabeza <br>Zombie 1",
-    popularity: 5,
-    price: 40000,
-    sizes: ["s", "m", "l"],
-    type: "sweater",
-}];
-
-
-
-
-function printElements(data) {
-    data.forEach(element => {
-        const data = element;
-        const product = document.createElement("a"); //la a lo hace un link
-            product.innerHTML = `
-            <img class="shirt__product__img" src="${data.URLimagen}" alt="">
-            <div class="product__name">
-                <h3> ${data.name}</h3>
-                <h4 class ="product__price"> ${data.price}</h4>
-                </div>
-            `;
-            product.classList.add("product");
-            product.setAttribute("href","#")
-
-            list.appendChild(product);
+const handleCollectionResult = (querySnapshot) => {
+    document.querySelectorAll('.list').forEach(item => item.innerHTML = '');
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      const product = document.createElement('a');
+      product.innerHTML = `
+        <img class="shirt__product__img" src="${data.URLimagen}" alt="">
+        <div class="product__name">
+            <h3> ${data.name}</h3>
+            <h4 class ="product__price"> ${data.price}</h4>
+            </div>
+        `;
+      document.querySelector(`.list-${data.type}`).appendChild(product);
     });
-}
+  }
 
-printElements(products);
+  const filters = document.querySelector('.filters');
+filters.addEventListener('change', function () {
+  let productsCollection = db.collection('products');
 
-filterOptions.addEventListener("change", function (){ //inicio filtros
-    console.log(filterOptions.type.value);
+  const types = [];
+  filters.productFilter.forEach(function (checkbox) {
+    if(checkbox.checked) {
+      types.push(checkbox.getAttribute('data-type'));
+    }
+  });
 
-    db.collection("products")
-    .where("type","==", filterOptions.type.value)
-    .get()
-    .then(handleCollectionResult);
+  if(types.length > 0){
+    productsCollection = productsCollection.where('type', 'in', types);
+  }
+
+  if(filters.type.value) {
+    productsCollection = productsCollection.where('type', '==', filters.type.value);
+  }
+
+  if(filters.price.value) {
+    switch(filters.price.value) {
+      case '0':
+        productsCollection = productsCollection.where('price', '<', 20000);
+        break;
+      case '1':
+        productsCollection = productsCollection.where('price', '<', 30000);
+        break;
+      case '2':
+        productsCollection = productsCollection.where('price', '>=', 20000);
+        break;
+      case '3':
+        productsCollection = productsCollection.where('price', '>=', 30000);
+      break;
+    }
+  }
+
+  if(filters.order.value) {
+    switch(filters.order.value) {
+      case 'price_asc':
+        productsCollection = productsCollection.orderBy('price', 'asc');
+        break;
+      case 'price_desc':
+        productsCollection = productsCollection.orderBy('price', 'desc');
+        break;
+    }
+  }
+
+  if(filters.popularity.value){
+    switch(filters.popularity.value){
+      case 'hola':
+        productsCollection = productsCollection.orderBy('popularity', 'asc');
+      break;
+
+      case 'chao':
+        productsCollection = productsCollection.orderBy('popularity', 'desc');
+      break;
+    }
+  }
+
+  productsCollection.get().then(handleCollectionResult);
 });
+
+let productsCollection = db.collection('products');
+
+productsCollection.get().then(handleCollectionResult)
 
